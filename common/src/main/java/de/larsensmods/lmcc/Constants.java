15c55185587e44355e3008aeee3dc9cd2a87dcd3
Larsen Rektor <larsen@lrektor.de>
2025-12-07 02:37:32 +0100

    Initial Commit

---
+++ common/src/main/java/de/larsensmods/lmcc/Constants.java
@@ -0,0 +1,11 @@
+package de.larsensmods.lmcc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "lmcc";
+    public static final String MOD_NAME = "LMCommonCore";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/de/larsensmods/lmcc/platform/FabricPlatformHelper.java
@@ -0,0 +1,22 @@
+package de.larsensmods.lmcc.platform;
+
+import de.larsensmods.lmcc.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
